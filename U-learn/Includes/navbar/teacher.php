59287b271a0d88cac0e53094ee23c5eee3327556
Esraa Saleem <es1915@live.com>
2021-03-02 21:39:00 +0200

    modify navbar student and teacher

--- U-learn/Includes/navbar/teacher.php
+++ U-learn/Includes/navbar/teacher.php
@@ -7,9 +7,9 @@
 
         <ul class="flex justify-between">
             <div class="flex">
-                <li class="list-border-b relative"><a href="">المرحلة الدراسية</a></li>
-                <li class="relative mx-12"><a href="">الكتب الدراسية</a></li>
-                <li class="relative"><a href="">الدروس</a></li>
+                <li class="list-border-b relative"><a href="Stage.php">المرحلة الدراسية</a></li>
+                <li class="relative mx-12"><a href="Book.php">الكتب الدراسية</a></li>
+                <li class="relative"><a href="index.php">الدروس</a></li>
             </div>
             <div class="flex items-center relative">
                 <li class="text-main-color font-medium text-xl"><a href="notify.php"><i class="far fa-bell"></i></a></li>
